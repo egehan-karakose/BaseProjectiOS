6818c5852c7a9c8c87ca79269a0b31577239f8c5
Egehan Karaköse <egehankarakose25@hotmail.com>
2023-04-17 18:13:48 +0300

    redundant code remove

--- BaseProject/AppDelegate.swift
+++ BaseProject/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        BaseDefaults.setup(with: .production)
+        AppDefaults.setup(with: .production)
         System.setup()
         Environment.shared.configure()
        
